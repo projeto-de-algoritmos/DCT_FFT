114af2f7b4c7d1f805ff41818354d0c4f4808074
Leonardo M. Miranda <leonardomichalskim@gmail.com>
2021-04-12 19:01:09 -0300

    add imprecise fft

--- fft.cpp
+++ fft.cpp
@@ -35,36 +35,55 @@ struct ccomplex* dft(struct ccomplex* x, const size_t N) {
 }
 
 
-// struct ccomplex* fft(struct ccomplex* x, const size_t N) {
-//   if (N % 2 > 0) {
-//     throw std::invalid_argument("Must be a power of 2");
-//   } else if (N <= 2) {
-//     return dft(x, N);
-//   } else {
-//     struct ccomplex* y = (struct ccomplex*) malloc(sizeof(struct ccomplex) * N);
-//     struct ccomplex* x_even = (struct ccomplex*) malloc(
-//       sizeof(struct ccomplex) * N/2);
-//     struct ccomplex* x_odd = (struct ccomplex*) malloc(
-//       sizeof(struct ccomplex) * N/2);
-//
-//     for (k = 0; k < N; k += 2) {
-//       x_even[k] = complex<double>(x[k].re, x[k].im);
-//       x_odd[k+1] = complex<double>(x[k+1].re, x[k+1].im);
-//       // x_odd[k+1] = x[k+1];
-//     }
-//     // TODO(leommiranda): separate x into odds and evens
-//     x_even = fft(x_even, N);
-//     x_odd = fft(x_odd, N);
-//     size_t k;
-//     size_t k2;
-//     for (k = 0; k < (size_t) N/2; k++) {
-//       y[k] = x_even[k] + (x_odd[k] * exp(complex<double>(0, -2*M_PI*k/N)));
-//       k2 = (size_t) k + N/2;
-//       y[k2] = x_even[k] + (x_odd[k] * exp(complex<double>(0, -2*M_PI*k2/N)));
-//     }
-//     return y;
-//   }
-// }
+struct ccomplex* fft(struct ccomplex* x, const size_t N) {
+  if (N % 2 > 0) {
+    throw std::invalid_argument("Must be a power of 2");
+  } else if (N <= 2) {
+    return dft(x, N);
+  } else {
+    size_t k;
+    size_t half_N = (size_t) (N/2);
+
+    struct ccomplex* y = (struct ccomplex*) malloc(sizeof(struct ccomplex) * N);
+    struct ccomplex* x_even = (struct ccomplex*) malloc(
+      sizeof(struct ccomplex) * half_N);
+    struct ccomplex* x_odd = (struct ccomplex*) malloc(
+      sizeof(struct ccomplex) * half_N);
+
+    for (k = 0; k < N; k += 2) {
+      x_even[k].re = x[k].re;
+      x_even[k].im = x[k].im;
+      x_odd[k+1].re = x[k+1].re;
+      x_odd[k+1].im = x[k+1].im;
+    }
+    x_even = fft(x_even, half_N);
+    x_odd = fft(x_odd, half_N);
+
+    size_t k2;
+    complex<double> x_odd_k;
+    complex<double> x_even_k;
+    complex<double> y_k_cpp;
+    complex<double> y_k2_cpp;
+    for (k = 0; k < (size_t) N/2; k++) {
+      x_odd_k = complex<double>(x_odd[k].re, x_odd[k].im);
+      x_even_k = complex<double>(x_even[k].re, x_even[k].im);
+      y_k_cpp = x_even_k + (x_odd_k * exp(complex<double>(0, -2*M_PI*k/N)));
+      y[k].im = y_k_cpp.imag();
+      y[k].re = y_k_cpp.real();
+
+      k2 = (size_t) k + N/2;
+      y_k2_cpp = x_even_k + (x_odd_k * exp(complex<double>(0, -2*M_PI*k2/N)));
+      y[k2].im = y_k2_cpp.imag();
+      y[k2].re = y_k2_cpp.real();
+    }
+    // TODO(leommiranda): dealloc x_even and x_odd
+    // for (k = 0; k < (size_t) N/2; k++) {
+      // free(x_even[k]);
+      // free(x_odd[k]);
+    // }
+    return y;
+  }
+}
 
 
 int main() {
@@ -80,9 +99,13 @@ int main() {
   }
 
   y = dft(x, 128);
-  // memcpy(&track->sig[0], &det->sig[0], 300U * sizeof(double));
-  // memcpy(y[0], dft(x, 128)[0], 5 * sizeof *arr);
+  printf("\n[");
+  for (k = 0; k < 128; k++) {
+    printf("(%.6lf, %.6lf), ", y[k].re, y[k].im);
+  }
+  printf("]\n");
 
+  y = fft(x, 128);
   printf("\n[");
   for (k = 0; k < 128; k++) {
     printf("(%.6lf, %.6lf), ", y[k].re, y[k].im);
